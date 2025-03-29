32585ba00838427720e518eb1fc392aab34894be
AshishH123H <ashish.pal@vishleshan.net>
2025-03-29 14:12:40 +0530

    `Added app-header component to app.component.html and updated imports in app.component.ts`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet]
+  imports: [CommonModule,HeaderComponent]
 })
 export class AppComponent {
  
